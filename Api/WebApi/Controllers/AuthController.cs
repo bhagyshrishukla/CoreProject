a051385ce4ee6bb11b49bf50eb2d5cb48ac9c534
bhagyshrishukla <bhagyshritech@gmail.com>
2021-04-14 15:50:33 +0530

    ,u first commit

---
+++ Api/WebApi/Controllers/AuthController.cs
@@ -0,0 +1,95 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace WebApi.Controllers
+{
+    [Authorize]
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        private readonly IJWTAuthenticationManager jWTAuthenticationManager;
+
+        public AuthController(IJWTAuthenticationManager jWTAuthenticationManager)
+        {
+            this.jWTAuthenticationManager = jWTAuthenticationManager;
+        }
+
+
+        // GET: api/Name
+        [HttpGet]
+        public IEnumerable<string> Get()
+        {
+            return new string[] { "New York", "New Jersey" };
+        }
+
+        // GET: api/Name/5
+        [HttpGet("{id}", Name = "Get")]
+        public string Get(int id)
+        {
+            return "New Jersey";
+        }
+
+        [AllowAnonymous]
+        [HttpPost("authenticate")]
+        public IActionResult Authenticate([FromBody] UserCred userCred)
+        {
+            var token = jWTAuthenticationManager.Authenticate(userCred.Username, userCred.Password);
+
+            if (token == null)
+                return Unauthorized();
+
+            return Ok(token);
+        }
+
+        //// GET: api/Name
+        //[HttpGet]
+        //public IEnumerable<string> Get()
+        //{
+        //    return new string[] { "value1", "value2" };
+        //}
+
+        //// GET: api/Name/5
+        //[HttpGet("{id}", Name = "Get")]
+        //public string Get(int id)
+        //{
+        //    return "value";
+        //}
+
+        //[HttpPost("authenticate")]
+        //public IActionResult Authenticate([FromBody] UserCred userCred)
+        //{
+        //    return Ok();
+        //}
+
+        public class UserCred
+        {
+            public string Username { get; set; }
+            public string Password { get; set; }
+        }
+
+        //// POST api/<AuthController>
+        //[HttpPost]
+        //public void Post([FromBody] string value)
+        //{
+        //}
+
+        //// PUT api/<AuthController>/5
+        //[HttpPut("{id}")]
+        //public void Put(int id, [FromBody] string value)
+        //{
+        //}
+
+        //// DELETE api/<AuthController>/5
+        //[HttpDelete("{id}")]
+        //public void Delete(int id)
+        //{
+        //}
+    }
+}
---
+++ Microservice - Copy/Microservice/Database/DatabaseContext.cs
@@ -0,0 +1,19 @@
+﻿using Microservice.Database.Entities;
+using Microsoft.EntityFrameworkCore;
+//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Threading.Tasks;
+
+namespace Microservice.Database
+{
+    public class DatabaseContext:DbContext
+    {
+        public DbSet<User> Users { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer(@"Data Source=(LocalDb)\MSSQLLocalDB;Initial Catalog=DemoTest;Integrated Security=True");
+          //base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
